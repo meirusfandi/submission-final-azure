58e45da9133d93a08c53807979d0309b88bd8160
meirusfandi <meirusfandi100@gmail.com>
2019-05-02 18:07:40 +0700

    add index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Final Submission | Azure Developer Academy</title>
+</head>
+<body>
+    <h2>Image List From Blob Storage</h2>
+    <a href="upload.php"><button>Add Image</button></a>
+    <hr>
+    <table>
+        <thead>
+            <th>No. </th>
+            <th>File Name</th>
+            <th>File Url</th>
+            <th>Preview</th>
+            <th>Action</th>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>Analyze</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
