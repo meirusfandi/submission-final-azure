a802c0d8d85d2cb5d883384f998dc83afb5e157c
meirusfandi <meirusfandi100@gmail.com>
2019-05-02 18:46:31 +0700

    update upload.php

--- upload.php
+++ upload.php
@@ -1,5 +1,20 @@
-<!-- PHP section -->
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Upload Image</title>
+</head>
+<body>
+    <h2>Upload Image to Storage</h2>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="image">
+        <input type="submit" name="upload" value="Upload Image">
+    </form>
+
+    <!-- PHP section -->
+    <?php
         require_once 'vendor/autoload.php';
         
         use MicrosoftAzure\Storage\Blob\BlobRestProxy;
@@ -29,20 +44,5 @@
             }
         }
     ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Upload Image</title>
-</head>
-<body>
-    <h2>Upload Image to Storage</h2>
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="image">
-        <input type="submit" name="upload" value="Upload Image">
-    </form>
 </body>
 </html>
