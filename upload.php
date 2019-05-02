fe5a0762acce52d39f42aab95e2acabeed96f035
meirusfandi <meirusfandi100@gmail.com>
2019-05-02 18:44:48 +0700

    relocation php section

--- upload.php
+++ upload.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Upload Image</title>
-</head>
-<body>
-
-    <!-- PHP section -->
-    <?php
+<!-- PHP section -->
+<?php
         require_once 'vendor/autoload.php';
         
         use MicrosoftAzure\Storage\Blob\BlobRestProxy;
@@ -39,6 +29,16 @@
             }
         }
     ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Upload Image</title>
+</head>
+<body>
     <h2>Upload Image to Storage</h2>
     <form action="upload.php" method="post" enctype="multipart/form-data">
         <input type="file" name="image">
