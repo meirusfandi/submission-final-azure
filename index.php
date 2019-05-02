9329993245d4666a174a2cd0be2292dcb72f6e22
meirusfandi <meirusfandi100@gmail.com>
2019-05-02 18:54:30 +0700

    add jquery and update

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Final Submission | Azure Developer Academy</title>
+    <script src="jquery.min.js"></script>
 </head>
 <body>
     <h2>Image List From Blob Storage</h2>
@@ -29,5 +30,71 @@
             </tr>
         </tbody>
     </table>
+
+    <hr>
+
+    <!-- Form Show response from Computer Vision -->
+    <h2>Show Respon From Analyze | Computer Vision</h2>
+    <div class="col-md-12">
+        <div id="wrapper" style="width:1020px; display:table;">
+            <div id="jsonOutput" style="width:600px; display:table-cell;">
+                Response:
+                <br><br>
+                <textarea id="responseTextArea" class="UIInput"
+                style="width:580px; height:400px;"></textarea>
+            </div>
+            <div id="imageDiv" style="width:420px; display:table-cell;">
+                Source image:
+                <br><br>
+                <img id="sourceImage" width="400" />
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript"> 
+        function processImage(){
+            var subscriptionKey = "2e2671970d6b469399ac05285a925f3e";
+            var uriBase = "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
+            var params = {
+                "visualFeatures": "Categories,Description,Color",
+                "details": "",
+                "language": "en",
+            };
+            // Display the image.
+            var sourceImageUrl = document.getElementById("imageUrl").value;
+            document.querySelector("#sourceImage").src = sourceImageUrl;
+            
+            // Make the REST API call.
+            $.ajax({
+                url: uriBase + "?" + $.param(params),
+    
+                // Request headers.
+                beforeSend: function(xhrObj){
+                    xhrObj.setRequestHeader("Content-Type","application/json");
+                    xhrObj.setRequestHeader(
+                        "Ocp-Apim-Subscription-Key", subscriptionKey);
+                },
+    
+                type: "POST",
+    
+                // Request body.
+                data: '{"url": ' + '"' + sourceImageUrl + '"}',
+            })
+    
+            .done(function(data) {
+                // Show formatted JSON on webpage.
+                $("#responseTextArea").val(JSON.stringify(data, null, 2));
+            })
+    
+            .fail(function(jqXHR, textStatus, errorThrown) {
+                // Display error message.
+                var errorString = (errorThrown === "") ? "Error. " :
+                    errorThrown + " (" + jqXHR.status + "): ";
+                errorString += (jqXHR.responseText === "") ? "" :
+                    jQuery.parseJSON(jqXHR.responseText).message;
+                alert(errorString);
+            });
+        };
+    </script>
 </body>
 </html>
