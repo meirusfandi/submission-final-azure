c6aa2552855ba9b15124ad52a4ca510340bf8714
hanifatussadiah <sadiyahhanifatus@gmail.com>
2019-05-04 08:06:32 +0700

    fixing project

--- index.php
+++ index.php
@@ -1,122 +1,31 @@
-<?php
-echo "<!DOCTYPE html>";
-echo '<html lang="en">';
-echo "<head>";
-echo '<meta charset="UTF-8">';
-echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
-echo '<meta http-equiv="X-UA-Compatible" content="ie=edge">';
-echo "<title>Final Submission | Azure Developer Academy</title>";
-echo '<script src="jquery.min.js"></script>';
-echo "</head>";
-echo "<body>";
-echo "<h2>Image List From Blob Storage</h2>";
-echo '<a href="upload.php"><button>Add Image</button></a>';
-echo "<hr>";
-echo "<table>";
-echo "<thead>";
-echo "<th>No. </th>";
-echo "<th>File Name</th>";
-echo "<th>File Url</th>";
-echo "<th>Preview</th>";
-echo "<th>Action</th>";
-echo "</thead>";
-
-echo "<tbody>";
-    require_once 'vendor/autoload.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Azure Cloud Developer Academy</title>
+    <script src="jquery.min.js"></script>
+</head>
+<body>
+    <script type="text/javascript">
+        function processImage() {
             
-    use MicrosoftAzure\Storage\Blob\BlobRestProxy;
-    use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
-    use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
-    use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
-    use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
-
-    $connect = "DefaultEndpointsProtocol=https;AccountName=meirusfandiwev;AccountKey=vwhIwbU1kaFKEZMFWTd5ng21ux0PA8P8XRgUgo6atp8xbKPYFStk5vz+7/lTIG8SyZ/37LGfYqQxqbsX/EIwCQ==;EndpointSuffix=core.windows.net";
-    $containername = "meirusfandi";
-    $blobclient = BlobRestProxy::createBlobService($connect);
-
-    try {
-        if (!$blobclient->containerExists($containername)){
-            $createcontainer = new CreateContainerOptions();
-            $createcontainer->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
-
-            $createcontainer->addMetaData("key1", "value1");
-            $createcontainer->addMetaData("key2", "value2");
-
-            $blobclient->createContainer($containername, $createcontainer);
-        }
-    } catch(ServiceException $e){
-        $code = $e->getCode();
-        $error_message = $e->getMessage();
-        echo $code.": ".$error_message."<br />";
-    }catch(InvalidArgumentTypeException $e){
-        $code = $e->getCode();
-        $error_message = $e->getMessage();
-        echo $code.": ".$error_message."<br />";
-    }
-    $listblobs = new ListBlobsOptions();
-    $listblobs->setPrefix("");
-
-    $result = $blobclient->listBlobs($containername, $listblobs);
-
-                if (sizeof($result->getBlobs()) >0){
-                    do {
-                        $i = 0;
-                        foreach ($result->getBlobs() as $blob){
-                            echo "<tr>";
-                            echo "<td>".++$i."</td>";
-                            echo "<td>".$blob->getName()."</td>";
-                            echo "<td>".$blob->getUrl()."</td>";
-                            echo '<td><img src=""></td>';
-                            echo '<td>
-                                <input type="hidden" name="imageUrl" id="imageUrl" value="'.$blob->getUrl().'">
-                                <button onclick="proccessImage()">Analyze</button>
-                                </td>';
-                            echo "</tr>";
-                        } $listblobs->setContinuationToken($result->getContinuationToken());
-                    } while ($result->getContinuationToken());
-                } else {
-                    echo "<tr>";
-                    echo '<td colspan="5">No Data on Storage</td>';
-                    echo "</tr>";
-                }
-                    
-        echo "</tbody>";
-    echo "</table>";
-
-    echo "<hr>";
-
-    //<!-- Form Show response from Computer Vision -->
-    echo "<h2>Show Respon From Analyze | Computer Vision</h2>";
-    echo '<div class="col-md-12">';
-    echo '<div id="wrapper" style="width:1020px; display:table;">';
-    echo '<div id="jsonOutput" style="width:600px; display:table-cell;">';
-    echo "      Response:";
-    echo "      <br><br>";
-    echo '      <textarea id="responseTextArea" class="UIInput"';
-    echo '      style="width:580px; height:400px;"></textarea>';
-    echo "  </div>";
-    echo '  <div id="imageDiv" style="width:420px; display:table-cell;">';
-    echo "      Source image:";
-    echo "      <br><br>";
-    echo '      <img id="sourceImage" width="400" />';
-    echo "  </div>";
-    echo "</div>";
-    echo "</div>";
+            var subscriptionKey = "<subscriptionKey>";
     
-    echo '
-    <script type="text/javascript"> 
-        function processImage(){
-            var subscriptionKey = "2e2671970d6b469399ac05285a925f3e";
             var uriBase = "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
+    
+            // Request parameters.
             var params = {
                 "visualFeatures": "Categories,Description,Color",
                 "details": "",
                 "language": "en",
             };
+    
             // Display the image.
-            var sourceImageUrl = document.getElementById("imageUrl").value;
+            var sourceImageUrl = document.getElementById("inputImage").value;
             document.querySelector("#sourceImage").src = sourceImageUrl;
-            
+    
             // Make the REST API call.
             $.ajax({
                 url: uriBase + "?" + $.param(params),
@@ -131,7 +40,7 @@ echo "<tbody>";
                 type: "POST",
     
                 // Request body.
-                data: {"url": + " + sourceImageUrl + "},
+                data: '{"url": ' + '"' + sourceImageUrl + '"}',
             })
     
             .done(function(data) {
@@ -148,8 +57,129 @@ echo "<tbody>";
                 alert(errorString);
             });
         };
-    </script>';
-echo "</body>";
-echo "</html>";
+    </script>
 
-?>
+    <h2>Upload New Image Source</h2>
+    <hr>
+    <form action="index.php" method="post" enctype="multipart/form-data">
+        <label for="fileUpload">File Source</label><br><br>
+        <input type="file" name="file" id="file"><br><br>
+        <input type="submit" name="upload" value="Upload Image"> | 
+        <input type="submit" name="load" value="Load File">
+    </form>
+
+    <!-- PHP Section -->
+    <?php 
+        require_once 'vendor/autoload.php';
+
+        use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+        use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+        use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+        use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+        use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+        
+        $connectionString = "DefaultEndpointsProtocol=https;AccountName=meirusfandiwev;AccountKey=vwhIwbU1kaFKEZMFWTd5ng21ux0PA8P8XRgUgo6atp8xbKPYFStk5vz+7/lTIG8SyZ/37LGfYqQxqbsX/EIwCQ==;EndpointSuffix=core.windows.net";
+        
+        // Create blob client.
+        $blobClient = BlobRestProxy::createBlobService($connectionString);
+
+        // Create container options object.
+        $createContainerOptions = new CreateContainerOptions();
+
+        $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+
+        // Set container metadata.
+        $createContainerOptions->addMetaData("key1", "value1");
+        $createContainerOptions->addMetaData("key2", "value2");
+
+        $containerName = "meirusfandi";
+
+        if (!$blobClient->containerExists($containerName)){
+            // Create container.
+            $blobClient->createContainer($containerName, $createContainerOptions);
+        }
+
+        if (isset($_POST['upload'])){
+            try {
+                // Getting local file so that we can upload it to Azure
+                $filename = strtolower($_FILES['file']['name']);
+                $filecontent = fopen($_FILES['file']['tmp_name'], "r");
+                
+                # Upload file as a block blob
+                echo "Uploading File: ".PHP_EOL;
+                echo $filename;
+                echo " was Successfully<br />";
+
+                //Upload blob
+                $blobClient->createBlockBlob($containerName, $filename, $filecontent);
+
+            } catch(ServiceException $e){
+                $code = $e->getCode();
+                $error_message = $e->getMessage();
+                echo $code.": ".$error_message."<br />";
+            } catch(InvalidArgumentTypeException $e){
+                $code = $e->getCode();
+                $error_message = $e->getMessage();
+                echo $code.": ".$error_message."<br />";
+            }
+        } else if (isset($_POST['load'])){
+
+            // List blobs.
+            $listBlobsOptions = new ListBlobsOptions();
+            $listBlobsOptions->setPrefix("");
+
+            echo "<hr>";
+            echo "<h2>Files on Blob Storage : </h2>";
+            echo "<table>";
+            echo "<thead>";
+            echo "<th>No. </th>";
+            echo "<th>File Name</th>";
+            echo "<th>File Url</th>";
+            echo "<th>Preview</th>";
+            echo "<th>Action</th>";
+            echo "</thead>";
+
+            echo "<tbody>";
+            $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+            if (sizeof($result->getBlobs()) > 0) {
+                $i = 0;
+                foreach ($result->getBlobs() as $blobFile) {
+                    echo "<tr>";
+                    echo "<td>".++$i."</td>";
+                    echo "<td>".$blobFile->getName()."</td>";
+                    echo "<td>".$blobFile->getUrl()."</td>";
+                    echo '<td width="200" height="200"><img src="'.$blobFile->getUrl().'" alt=""></td>';
+                    echo "<td>";
+                    echo '<input type="hidden" name="inputImage" id="inputImage" value="'.$blobFile->getUrl().'">';
+                    echo '<button onclick="processImage()">Analyze</button>';
+                    echo "</td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "<tr>";
+                echo '<td colspan="5">No data on this blob</td>';
+                echo "</tr>";
+            }
+            echo "</tbody>";
+            echo "</table>";
+            
+            echo "<hr>";
+            echo "<br><br>";
+            echo "<h2>Analyzing Image Using Computer Vision</h2>";
+            echo '<div id="wrapper" style="width:1020px; display:table;">';
+                echo '<div id="jsonOutput" style="width:600px; display:table-cell;">';
+                    echo "Response:";
+                    echo "<br><br>";
+                    echo '<textarea id="responseTextArea" class="UIInput" style="width:580px; height:400px;"></textarea>';
+                echo "</div>";
+                echo '<div id="imageDiv" style="width:420px; display:table-cell;">';
+                    echo "Source image:";
+                    echo "<br><br>";
+                    echo '<img id="sourceImage" width="400" />';
+                echo "</div>";
+            echo "</div>";
+        }
+    ?>
+    
+</body>
+</html>
